ad95970fe0b04508ff7406f9ef77c809fbde2b6d
Manthan <manthanrathod761@gmail.com>
2024-10-13 15:35:18 +0530

    Just Changes

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,7 +7,7 @@ const Contact = () => {
       <div style={{ textAlign: 'center',marginTop: "150px" }}>
         <h2>Contact Us</h2>
         <p>Customer Care</p>
-        <p>Phone: 1-800-555-5555</p>
+        <p>Phone: 1-800-555-5585</p>
         <p>Email: support@foodbazzar.com</p>
         <p>Address : Main Branch New Delhi Nehru Road</p>
         <p>Thank You</p>
